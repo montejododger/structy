eae14e5b21bf5f3a94b4b4e854f562501e26d179
Matthew Montejo <montejododger@yahoo.com>
2024-01-11 10:55:34 -0800

    compress

--- 01-array-and-strings.js
+++ 01-array-and-strings.js
@@ -1,4 +1,5 @@
 // 01. Array and String
+// ############################################################################################################################################
 
 // uncompress("2c3a1t"); // -> 'ccaaat'
 // uncompress("4s2b"); // -> 'ssssbb'
@@ -47,5 +48,79 @@ const uncompress = (s) => {
 };
 
 // ############################################################################################################################################
+
+/*  TESTS
+compress('ccaaatsss'); // -> '2c3at3s'
+compress('ssssbbz'); // -> '4s2bz'
+compress('ppoppppp'); // -> '2po5p'
+compress('nnneeeeeeeeeeeezz'); // -> '3n12e2z'
+compress('yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy');
+-> '127y'
+*/
+
+/*  GAME PLAN
+    create a new arr
+    create two pointers
+    while loop to iterate thru (s)
+    check if i === j
+    if it doesnt check if num === 1
+    push char into new arr if === 1
+    else push num and char into arr
+    return a joined new arr
+*/
+
+const compress = (s) => {
+    const newWord = [];
+
+    let i = 0;
+    let j = 0;
+
+    while (j < s.length) {
+        if (s[i] === s[j]) {
+            j++;
+        } else {
+            const num = j - i;
+            if (num === 1) {
+                newWord.push(s[i]);
+            } else {
+                newWord.push(String(num), s[i]);
+            }
+
+            i = j;
+        }
+    }
+
+    return newWord.join("");
+};
+
+// ############################################################################################################################################
+
+/*  TESTS
+
+anagrams('restful', 'fluster'); // -> true
+anagrams('cats', 'tocs'); // -> false
+anagrams('monkeyswrite', 'newyorktimes'); // -> true
+anagrams('paper', 'reapa'); // -> false
+anagrams('elbow', 'below'); // -> true
+anagrams('tax', 'taxi'); // -> false
+anagrams('taxi', 'tax'); // -> false
+anagrams('night', 'thing'); // -> true
+anagrams('abbc', 'aabc'); // -> false
+anagrams('po', 'popp'); // -> false
+anagrams('pp', 'oo') // -> false
+
+*/
+
+
+/* GAME PLAN
+
+
+*/
+
+
+let anagrams = (s1, s2) => {
+
+}
+
 
 // ############################################################################################################################################

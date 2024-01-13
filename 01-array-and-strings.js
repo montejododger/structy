1ea946773e0daaac1074802e5bcb85dc2a32693b
Matthew Montejo <montejododger@yahoo.com>
2024-01-13 13:03:05 -0800

    fiveSort

--- 01-array-and-strings.js
+++ 01-array-and-strings.js
@@ -306,14 +306,54 @@ const intersection = (a, b) => {
 
 /* TESTS
 
+fiveSort([12, 5, 1, 5, 12, 7]);
+// -> [12, 7, 1, 12, 5, 5]
 
+fiveSort([5, 2, 5, 6, 5, 1, 10, 2, 5, 5]);
+// -> [2, 2, 10, 6, 1, 5, 5, 5, 5, 5]
 
-*/
+fiveSort([5, 5, 5, 1, 1, 1, 4]);
+// -> [4, 1, 1, 1, 5, 5, 5]
+
+fiveSort([5, 5, 6, 5, 5, 5, 5]);
+// -> [6, 5, 5, 5, 5, 5, 5]
+
+fiveSort([5, 1, 2, 5, 5, 3, 2, 5, 1, 5, 5, 5, 4, 5]);
+// -> [4, 1, 2, 1, 2, 3, 5, 5, 5, 5, 5, 5, 5, 5]
+
+const fives = new Array(20000).fill(5);
+const fours = new Array(20000).fill(4);
+const nums = [...fives, ...fours];
+fiveSort(nums);
+// twenty-thousand 4s followed by twenty-thousand 5s
+// -> [4, 4, 4, 4, ..., 5, 5, 5, 5]
 
 /* GAME PLAN
 
-
+    create 2 points
+    start at the edn and check if its 5 or not
+    once its 5 then check if the start num i  5 and if so then swap and increment start
+    if start isnt a 5, increment 
 
 */
+
+let fiveSort = (nums) => {
+
+    let i = 0;
+    let j = nums.length - 1;
+
+    while (i < j) {
+        if (nums[j] === 5) {
+            j--;
+        } else if (nums[i] === 5) {
+            [nums[i], nums[j]] = [nums[j], nums[i]];
+            i++;
+        } else {
+            i++;
+        }
+    }
+
+    return nums;
+};
 
 // ############################################################################################################################################

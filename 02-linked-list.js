0c2ad1d082bd69b5ea1fc259e9b7d6e8de9c2065
Matthew Montejo <montejododger@yahoo.com>
2024-01-22 14:59:15 -0800

    zipperedList

--- 02-linked-list.js
+++ 02-linked-list.js
@@ -279,7 +279,7 @@ const getNodeValue = (head, index) => {
         count++;
     }
 
-    return null
+    return null;
 };
 
 // ############################################################################################################################################
@@ -330,7 +330,6 @@ reverseList(p); // p
 //  prev    curr      next
 
 const reverseList = (head) => {
-
     let prev = null;
     let current = head;
 
@@ -338,12 +337,129 @@ const reverseList = (head) => {
         const next = current.next; // so we dont lose the pointer to the next node
 
         current.next = prev;
-        prev = current
+        prev = current;
         current = next;
     }
 
     return prev;
-}
+};
+
+// ############################################################################################################################################
+
+/* TESTS
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+a.next = b;
+b.next = c;
+// a -> b -> c
+
+const x = new Node("x");
+const y = new Node("y");
+const z = new Node("z");
+x.next = y;
+y.next = z;
+// x -> y -> z
+
+zipperLists(a, x);
+// a -> x -> b -> y -> c -> z
+
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+const e = new Node("e");
+const f = new Node("f");
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
+e.next = f;
+// a -> b -> c -> d -> e -> f
+
+const x = new Node("x");
+const y = new Node("y");
+const z = new Node("z");
+x.next = y;
+y.next = z;
+// x -> y -> z
+
+zipperLists(a, x);
+// a -> x -> b -> y -> c -> z -> d -> e -> f
+
+const s = new Node("s");
+const t = new Node("t");
+s.next = t;
+// s -> t
+
+const one = new Node(1);
+const two = new Node(2);
+const three = new Node(3);
+const four = new Node(4);
+one.next = two;
+two.next = three;
+three.next = four;
+// 1 -> 2 -> 3 -> 4
+
+zipperLists(s, one);
+// s -> 1 -> t -> 2 -> 3 -> 4
+
+const w = new Node("w");
+
+// w
+
+const one = new Node(1);
+const two = new Node(2);
+const three = new Node(3);
+one.next = two;
+two.next = three;
+// 1 -> 2 -> 3
+
+zipperLists(w, one);
+// w -> 1 -> 2 -> 3
+
+const one = new Node(1);
+const two = new Node(2);
+const three = new Node(3);
+one.next = two;
+two.next = three;
+// 1 -> 2 -> 3
+
+const w = new Node("w");
+// w
+
+zipperLists(one, w);
+// 1 -> w -> 2 -> 3
+*/
+
+/*  GAME PLAN
+
+*/
+
+let zipperLists = (head1, head2) => {
+    let tail = head1;
+    let current1 = head1.next;
+    let current2 = head2;
+    let flag = false;
+
+    while (current1 !== null && current2 !== null) {
+        if (flag === false) {
+            tail.next = current2;
+            current2 = current2.next;
+        } else {
+            tail.next = current1;
+            current1 = current1.next;
+        }
+
+        tail = tail.next;
+        flag = !flag;
+    }
+
+    if (current1 !== null) tail.next = current1;
+    if (current2 !== null) tail.next = current2;
+
+    return head1;
+};
 
 // ############################################################################################################################################
 
@@ -354,23 +470,10 @@ const reverseList = (head) => {
 /*  GAME PLAN
 
 */
-
-// ############################################################################################################################################
-
-/* TESTS
-
-*/
-
-/*  GAME PLAN
-
-*/
-
 
 //  N   ->  a   ->    b   ->   c   ->   d
 //  prev    curr      next
 
-
-
 // ############################################################################################################################################
 
 /* TESTS
@@ -380,7 +483,6 @@ const reverseList = (head) => {
 /*  GAME PLAN
 
 */
-
 
 //  N   ->  a   ->    b   ->   c   ->   d
 //  prev    curr      next

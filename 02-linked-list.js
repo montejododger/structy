2eb616151331cc2fa44a7d8368414e6ee9fc8296
Matthew Montejo <montejododger@yahoo.com>
2024-01-30 09:01:12 -0800

    addLists recursive

--- 02-linked-list.js
+++ 02-linked-list.js
@@ -1214,6 +1214,26 @@ const addLists = (head1, head2) => {asdflkjh
 };
 
 // Recursive
+const addLists = (head1, head2, carry = 0) => {
+    // base case
+    if (head1 === null && head2 === null && carry === 0) return null;
+
+    const val1 = head1 === null ? 0 : head1.val;
+    const val2 = head2 === null ? 0 : head2.val;
+
+    const sum = val1 + val2 + carry;
+    const nextCarry = sum > 9 ? 1 : 0;
+    const digit = sum % 10;
+
+    const resultNode = new Node(digit);
+
+    const next1 = head1 === null ? null : head1.next;
+    const next2 = head2 === null ? null : head2.next;
+
+    resultNode.next = addLists(next1, next2, nextCarry);
+
+    return resultNode;
+};
 
 
 // ############################################################################################################################################

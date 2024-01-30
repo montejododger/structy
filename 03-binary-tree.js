5bb0b2d1461e3557dc9bb2ecbbf53e77b134c510
Matthew Montejo <montejododger@yahoo.com>
2024-01-30 12:15:55 -0800

    depthFirstValues

--- 03-binary-tree.js
+++ 03-binary-tree.js
@@ -1,5 +1,16 @@
 // 3. BINARY TREE
+
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.left = null;
+//     this.right = null;
+//   }
+// }
+
 // ############################################################################################################################################
+
+// STACK
 
 /* TESTS
 
@@ -8,6 +19,51 @@
 /*  GAME PLAN
 
 */
+
+// ITERIVE
+
+let depthFirstValues = (root) => {
+    if (root === null) return [];
+
+    const values = [];
+    const stack = [root];
+
+    while (stack.length > 0) {
+        const node = stack.pop();
+        values.push(node.val);
+
+        // adding the right first puts the left at the top of stack
+        if (node.right) stack.push(node.right);
+        if (node.left) stack.push(node.left);
+    }
+
+    return values;
+};
+
+/*
+                [a]  -> [c, b] -> [c, e, d] -> [c, e] -> [c]    ->  [f]
+                pops a  pops b -> pops d    -> pops e -> pops c ->
+
+                -> ['a', 'b', 'd', 'e', 'c', 'f']
+     a
+   /   \
+  b     c
+ / \     \
+d   e     f
+
+
+*/
+
+// RECURSIVELY
+
+let depthFirstValues = (root) => {
+    if (!root) return [];
+    const left = depthFirstValues(root.left);
+    const right = depthFirstValues(root.right);
+
+    return [root.val, ...left, ...right];
+};
+
 // ############################################################################################################################################
 
 /* TESTS

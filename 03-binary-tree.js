f1db9ebe4716c9d15e3ae74a3d6a5d5c7e3570a2
Matthew Montejo <montejododger@yahoo.com>
2024-02-16 08:39:04 -0800

    leafList

--- 03-binary-tree.js
+++ 03-binary-tree.js
@@ -472,3 +472,18 @@ const avg = (array) => {
 /*  GAME PLAN
 
 */
+
+const leafList = (root) => {
+    if (root === null) return [];
+    const leaves = [];
+    const stack = [root];
+    while (stack.length) {
+        const current = stack.pop();
+        if (current.left === null && current.right === null)
+            leaves.push(current.val);
+
+        if (current.right !== null) stack.push(current.right);
+        if (current.left !== null) stack.push(current.left);
+    }
+    return leaves;
+};

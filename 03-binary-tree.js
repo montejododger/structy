42fcf460a48babe0cae9938a7aec20cdba45a5c1
Matthew Montejo <montejododger@yahoo.com>
2024-02-19 16:20:24 -0800

    treeLevels notes

--- 03-binary-tree.js
+++ 03-binary-tree.js
@@ -415,28 +415,56 @@ const allTreePaths = (root) => {
 /*  GAME PLAN
 
 */
+// Depth First
+
 const treeLevels = (root) => {
-    if (root === null) return [];
 
+    // edge case
+    if (!root) return [];
+
+    // create a 2-D array to store levels and return
     const levels = [];
-    const queue = [{ node: root, levelNum: 0 }];
-    while (queue.length > 0) {
-        const { node, levelNum } = queue.shift();
 
-        if (levels.length === levelNum) {
-            levels[levelNum] = [node.val];
-        } else {
-            levels[levelNum].push(node.val);
-        }
+    // create a astack with an object in it to help figure out the levels
+    const stack = [{ node: root, levelNum: 0 }];
 
-        if (node.left !== null)
-            queue.push({ node: node.left, levelNum: levelNum + 1 });
-        if (node.right !== null)
-            queue.push({ node: node.right, levelNum: levelNum + 1 });
+    // while loop till empty
+    while (stack.length > 0) {
+        // deconstruct the last object o nthe stack
+        const { node, levelNum } = stack.pop();
+
+        // check if the length equals the level; if it does then it doesnt exist and we need to create the sub array
+        // if it doesnt then we need to add the node the sub array that already exist
+        levels.length === levelNum ? levels[levelNum] = [node.val] : levels[levelNum].push(node.val);
+
+        if (node.right) stack.push({ node: node.right, levelNum: levelNum + 1 });
+        if (node.left)stack.push({ node: node.left, levelNum: levelNum + 1 });
     }
-
     return levels;
 };
+// const treeLevels = (root) => {
+//     if (root === null) return [];
+
+//     const levels = [];
+//     const queue = [{ node: root, levelNum: 0 }];
+//     while (queue.length > 0) {
+//         const { node, levelNum } = queue.shift();
+
+//         if (levels.length === levelNum) {
+//             levels[levelNum] = [node.val];
+//         } else {
+//             levels[levelNum].push(node.val);
+//         }
+
+//         if (node.left !== null)
+//             queue.push({ node: node.left, levelNum: levelNum + 1 });
+//         if (node.right !== null)
+//             queue.push({ node: node.right, levelNum: levelNum + 1 });
+//     }
+
+//     return levels;
+// };
+
 // ############################################################################################################################################
 
 /* TESTS
